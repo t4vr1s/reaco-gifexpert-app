07a028cf8f38b864f9b1ccd0772cd38e74cb02e3
ees <eescobar@computin.cl>
2020-08-06 20:41:25 -0400

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
+
+export const useFetchGifs = (category) => {
+  const [state, setState] = useState({ data: [], loading: true });
+
+  useEffect(() => {
+    getGifs(category).then((gifs) => {
+      setState({
+        data: gifs,
+        loading: false,
+      });
+    });
+  }, [category]);
+
+  return state;
+};
